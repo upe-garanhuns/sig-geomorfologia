45dd8577957d4dea6387a27848483330d7a378b0
João Neto <jbsn1@discente.ifpe.edu.br>
2023-04-26 10:38:18 -0300

    feat: mensagem de erro em português

--- public/script/Gps.js
+++ public/script/Gps.js
@@ -3,3 +3,9 @@ L.control.locate({
        popup: "Você está dentro de um raio de {distance} metros deste ponto."
     }
 }).addTo(map);
+
+function onLocationError(e) {
+    alert("Para utilizar essa função, é necessário permitir a localização.");
+}
+
+map.on('locationerror', onLocationError);
